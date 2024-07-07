0edb89ba78bf7472abc7ca8a064602045bd36608
Wisdom <wisdomokwen@gmail.com>
2024-07-07 14:43:07 -0700

    Add SEO

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import NavBar from './components/navbar/Navbar';
 import Projects from './components/projects/Projects';
 import Footer from './components/footer/Footer';
+import SEO from './components/Seo';
 import Experiences from './components/experiences/Experiences';
 
 function App() {
   return (
     <div className='app'>
+      <SEO />
       <NavBar />
       <About />
       <Experiences />
---
+++ src/components/Seo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const SEO = () => (
+  <Helmet>
+    <title>Wisdom Okwen</title>
+    <meta name="description" content="Wisdom Okwen personal portfolio website showcasing experiences, projects and resume." />
+    <meta name="keywords" content="Wisdom Okwen, website, software engineer, projects, experiences" />
+    <meta name="robots" content="index, follow" />
+  </Helmet>
+);
+
+export default SEO;
