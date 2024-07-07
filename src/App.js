f3bfda7022bb7c89931a4bc8bcd50515ef3d9513
Wisdom <wisdomokwen@gmail.com>
2024-07-06 21:50:46 -0700

    push all

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import About from './components/about/About';
 import './App.css';
+import NavBar from './components/navbar/Navbar';
+import Projects from './components/projects/Projects';
+import Footer from './components/footer/Footer';
+import Experiences from './components/experiences/Experiences';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <NavBar />
+      <About />
+      <Experiences />
+      <Projects />
+      <Footer />
     </div>
   );
 }
