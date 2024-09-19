3c997d6cd3f160ba0613d8c148dfe838f9266c58
Wisdom Okwen <wokwen@unc.edu>
2024-09-19 15:52:38 -0400

    Change profile pic

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,7 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import GitHubIcon from '@mui/icons-material/GitHub';
-import about from '../../images/about.png';
+import profile_pic from '../../images/profile_pic.jpg';
 import './About.css';
 import { message, name } from './message';
 import Typewriter from './TypeWriter';
@@ -21,7 +21,7 @@ function About() {
     return (
         <div id='about'>
             <div className='left'>
-                <img src={ about } alt="profile" />
+                <img src={ profile_pic } alt="profile" />
                 <div className="socials">
                     <a href={socialMediaLinks.gitHub} target="_blank" rel="noreferrer"><GitHubIcon id="github"/></a> <span className='divider'></span>
                     <a href={socialMediaLinks.linkedIn} target="_blank" rel="noreferrer"><LinkedInIcon id="linkedin"/></a> <span className='divider'></span> 
